4e58c93e724ba90b0fd6295225d93036379a8963
Sulaiman Sanaullah <ssanaul20@gmail.com>
2017-11-16 17:05:22 -0600

    Update server.js to utilize secrets.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Comment = require('./model/comments');
+var secrets = require('./secrets');
 
 //and create our instances
 var app = express();
@@ -14,8 +15,8 @@ var router = express.Router();
 //set our port to either a predetermined port number if you have set it up, or 3001
 var port = process.env.API_PORT || 3001;
 
-//db config -- REPLACE USERNAME/PASSWORD/DATABASE WITH YOUR OWN FROM MLAB!
-var mongoDB = 'mongodb://<DBUSERNAME>:<DBPASSWORD>@ds019836.mlab.com:19836/bryandb';
+//db config -- set your URI from mLab in secrets.js
+var mongoDB = secrets.requestSecret('db_uri');
 mongoose.connect(mongoDB, { useMongoClient: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
